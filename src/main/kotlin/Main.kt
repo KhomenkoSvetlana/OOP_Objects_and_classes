99f62dce97c8d2b94ae4727429e436bd8fa5cc89
Svetlana Khomenko <homenko-svetlana87@mail.ru>
2023-07-14 09:30:12 +0300

    next commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,63 +1,81 @@
+import java.lang.IllegalArgumentException
 import java.util.Date
 
 data class Post(
     val id: Int, // Идентификатор записи
-    val ownerId: Int, // Id владельца стены
-    val fromId: Int, // Id автора записи
+    val ownerId: Int = 0, // Id владельца стены
+    val fromId: Int = 0, // Id автора записи
     val date: Date = Date(),
     val text: String,
-    val replyOwnerId: Int, // Id владельца записи, в ответ на которую была оставлена текущая
-    val replyPostId: Int, // Id записи, в ответ на которую была оставлена текущая
+    val replyOwnerId: Int = 0, // Id владельца записи, в ответ на которую была оставлена текущая
+    val replyPostId: Int = 0, // Id записи, в ответ на которую была оставлена текущая
     val friendsOnly: Boolean = false, // 1, если запись была создана с опцией «Только для друзей».
     val canPin: Boolean = false,
     val canDelete: Boolean = false,
     val likes: Likes? = Likes(),
-    val views: Views? = Views()
+    val views: Views? = Views(),
+    val comment: Comment? = Comment()
 )
 
-class Likes(
+data class Comment(
+    val postId: Int = 0,
+    val commentId: Int = 0,
+    val date: Date = Date(),
+    val text: String? = null
+)
+
+data class Likes(
     val count: Int = 0, // число пользователей, которым понравилась запись
     val userLikes: Boolean = false,
     val canLike: Boolean = true,
     val canPublish: Boolean = true
 )
 
-class Views(
+data class Views(
     val count: Int = 0 //число просмотров записи
 )
 
 object WallService {
     private var postsOnTheWall = emptyArray<Post>()
+    private var commentOnThePost = emptyArray<Comment>()
     private var lastId = 0
 
     fun add(post: Post): Post {
-        postsOnTheWall += post.copy(id = post.id + lastId)
+        postsOnTheWall += post.copy(id = post.id + lastId, likes = post.likes?.copy())
         return postsOnTheWall.last()
     }
 
     fun update(updatePost: Post): Boolean {
         for ((index, post) in postsOnTheWall.withIndex()) {
             if (post.id == updatePost.id) {
-                postsOnTheWall[index] = post.copy()
+                postsOnTheWall[index] = post.copy(likes = post.likes?.copy())
                 return true
             }
         }
         return false
     }
 
-    fun print(){
+    fun print() {
         for (post in postsOnTheWall) {
             println(post)
         }
         println()
     }
 
-}
+    fun createComment(postId: Int, comment: Comment): Comment {
+            for ((index, post) in postsOnTheWall.withIndex()) {
+                if (post.id == postId) {
+                    commentOnThePost [index] = comment.copy()
+                    return commentOnThePost.last()
+                }
+            }
+        return throw IllegalArgumentException("PostNotFoundException")
+        }
+    }
 
 fun main(args: Array<String>) {
-    WallService.add(Post(1, 0, 0, date = Date(), "First post", 5, 6))
-    WallService.add(Post(5, 4, 7, date = Date(), "Second post", 3, 6))
+    WallService.add(Post(1, text = "First post"))
+    WallService.add(Post(2, text = "Second post"))
     WallService.print()
-    WallService.add(Post(5, 4, 7, date = Date(), "First post", 3, 6))
-
+    WallService.createComment(1, comment = Comment(1, text = "comment"))
 }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -40,4 +40,5 @@ class WallServiceTest {
         assertFalse(result)
     }
 
+
 }
